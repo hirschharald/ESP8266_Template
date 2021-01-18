43d145dbe3c38e874a1a237030b469a96cf5f171
hirschharald <hirschharald@gmx.net>
2021-01-18 20:42:30 +0100

    Update index_page.h

--- index_page.h
+++ index_page.h
@@ -4,36 +4,6 @@ const char index_page[] PROGMEM = R"=====(
   <HEAD>
     <META name='viewport' content='width=device-width, initial-scale=1'>
     <TITLE>ESP Web Template</TITLE>
-    <SCRIPT>
-    var xmlHttp=createXmlHttpObject();
-    function createXmlHttpObject(){
-     if(window.XMLHttpRequest){
-        xmlHttp=new XMLHttpRequest();
-     }else{
-        xmlHttp=new ActiveXObject('Microsoft.XMLHTTP');
-     }
-     return xmlHttp;
-    }
-    function process(){
-      if(xmlHttp.readyState==0 || xmlHttp.readyState==4){
-        
-      }
-      setTimeout('process()',2000);
-    }
-    function handleServerResponse(){
-     if(xmlHttp.readyState==4 && xmlHttp.status==200){
-       xmlResponse=xmlHttp.responseXML;
-       for(i=0;i<5;i++){
-        xmldoc=xmlResponse.getElementsByTagName('response'+i)[0].firstChild.nodeValue;
-        document.getElementById('resp'+i).innerHTML=xmldoc;
-       }
-       xmldoc=xmlResponse.getElementsByTagName('freeh')[0].firstChild.nodeValue;
-       document.getElementById('freeheap').innerHTML=xmldoc;
-       xmldoc=xmlResponse.getElementsByTagName('rst')[0].firstChild.nodeValue;
-       document.getElementById('rstreason').innerHTML=xmldoc;
-     }
-    }
-    </SCRIPT>
     <STYLE>
       h1 {
         font-size: 120%;
@@ -42,7 +12,7 @@ const char index_page[] PROGMEM = R"=====(
       }
     </STYLE>
   </HEAD>
-  <BODY onload='process()'>
+  <BODY>
     <CENTER>
       <H1>Hello Web user</H1>
     </CENTER>
